193c0bfb6edbf33f513ea28801cdb16b267af71d
tungtrn <tungt1281@gmail.com>
2023-03-01 16:20:30 -0500

    Add firebase config, and authentication page

--- front/src/app/routes/routes.tsx
+++ front/src/app/routes/routes.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/HomePage/Home";
+import Login from "../pages/LoginPage/Login";
+
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home />
     },
+    {
+        path: "/login",
+        element: <Login />
+    }
 ]);
 
 export default router;
