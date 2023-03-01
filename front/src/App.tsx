2a7354d90213af2854fbf8827671858e9db4b17a
tungtrn <tungt1281@gmail.com>
2023-03-01 17:34:37 -0500

    Add Login function

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,32 @@
 import './App.css';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './app/routes/routes';
+import { logout, setUser } from './app/reducer/authSlice';
+import { useAppDispatch } from './app/hooks/hooks';
+import { auth } from './app/firebase-config';
+import { User } from './app/models/User';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const loggedInUser: User = {
+          name: user?.displayName || '',
+        };
+  
+        dispatch(setUser(loggedInUser));
+      } else {
+        dispatch(logout);
+      }
+    });
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <div>
       <RouterProvider router={router} />
