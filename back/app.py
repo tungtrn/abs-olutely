52e1669e36a8fb56dbc637686e9438bc2fca5e8b
Nam Dao <nhd36@drexel.edu>
2023-03-09 00:14:02 -0500

    Add authentication, set up data model

--- back/app.py
+++ back/app.py
@@ -1,27 +1,72 @@
-from flask import Flask
+from flask import Flask, jsonify
+from config import ConfigApp, Config
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-import firebase_admin
-from firebase_admin import firestore
-from firebase_admin import credentials
+from werkzeug.exceptions import InternalServerError
+import datetime
+from .models import db, User, IdentityCard, Token
 
 app = Flask(__name__)
 
-CORS(app)
+cors = CORS(app)
+jwt = JWTManager(app=app)
+db.init_app(app=app)
 
-# Initialize firestore
-cred = credentials.Certificate("credentials.json")
-firebase_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+app.config["SQLALCHEMY_DATABASE_URI"] = ConfigApp.connection
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = ConfigApp.upload_folder
+app.config["JWT_SECRET_KEY"] = ConfigApp.app_secret
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['CORS_HEADERS'] = ['Content-Type']
+app.config['CORS_ORIGINS'] = ['*']
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
+# Config application
 
-@app.route('/')
-def hello():
-    db.collection(u'cities').document(u'LA').set({
-        u'name': u'Los Angles',
-        u'state': u'CA',
-        u'country': u'USA'
+@app.errorhandler(500)
+def internal_error(e):
+    print(e)
+    message = {"message": Config.internal_error}
+    return message
+
+# Config JWT
+
+@jwt.expired_token_loader
+def my_expired_token_callback():
+    message = jsonify({
+        "message": Config.token_expired 
     })
-    return "My flask API"
+    return message, Config.HTTP_401_UNAUTHORIZED
+
+@jwt.unauthorized_loader
+def unauthorized_jwt_header(expired_token):
+    message = jsonify({
+        "message": Config.token_not_found
+    })
+
+    return message, Config.HTTP_401_UNAUTHORIZED
+
+@jwt.revoked_token_loader
+def revoked_token_handler():
+    message = jsonify({
+        "message": Config.revoked_token
+    })
+
+    return message, Config.HTTP_401_UNAUTHORIZED
+
+@jwt.token_in_blacklist_loader
+def check_token_in_blacklist(decrypted_token):
+    jti = decrypted_token["jti"]
+    return Token.check_token_blacklisted(jti)
+
+# Create new application
+
+from .api.auth import auth_bp
+
+app.register_error_handler(500, internal_error)
+
+app.register_blueprint(auth_bp, url_prefix="/api/v1/auth")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
